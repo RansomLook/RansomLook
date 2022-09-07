0003b69f0d7f038a27972b5cb8961ce10708de8f
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-09-07 13:49:02 +0200

    Code cleaning with pylint

--- ransomlook/ransomlook.py
+++ ransomlook/ransomlook.py
@@ -7,21 +7,22 @@ does what it says on the tin
 '''
 import os
 import json
-import argparse
 import queue
 from threading import Thread, Lock
 from datetime import datetime
 import time
-from typing import List, Dict
+from typing import Dict
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+
 from .default.config import get_config
 
 from .sharedutils import striptld
 from .sharedutils import openjson
 from .sharedutils import siteschema
-from .sharedutils import stdlog, dbglog, errlog, honk
+from .sharedutils import stdlog, errlog
 from .sharedutils import createfile
 
-from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+# pylint: disable=W0703
 
 def creategroup(name: str, location: str) -> Dict[str, object] :
     '''
@@ -35,7 +36,7 @@ def creategroup(name: str, location: str) -> Dict[str, object] :
         'locations': [
             mylocation
         ],
-        'profile': list()
+        'profile': []
     }
     return insertdata
 
@@ -49,74 +50,72 @@ def checkexisting(provider: str) -> bool:
             return True
     return False
 
-
-def threadscape(q, lock):
-    with sync_playwright() as p:
+def threadscape(queuethread, lock):
+    '''
+    Thread used to scrape our website
+    '''
+    with sync_playwright() as play:
         while True:
-            host, group = q.get()
+            host, group = queuethread.get()
             stdlog('Starting : ' + host['fqdn']+ ' --------- ' + group)
             host['available'] = bool()
-            '''
-            only scrape onion v3 unless using headless browser, not long before this will not be possible
-            https://support.torproject.org/onionservices/v2-deprecation/
-            '''
             try:
-               browser = p.chromium.launch(proxy={"server": "socks5://127.0.0.1:9050"}, args=['--unsafely-treat-insecure-origin-as-secure='+host['slug']])
-               context = browser.new_context(ignore_https_errors= True )
-               page = context.new_page()
-               if 'timeout' in host and host['timeout'] is not None:
-                  entry = page.goto(host['slug'], wait_until='load', timeout = host['timeout']*1000)
-               else:
-                  entry = page.goto(host['slug'], wait_until='load', timeout = 120000)
-               page.bring_to_front()
-               delay = host['delay']*1000 if ( 'delay' in host and host['delay'] is not None ) else 15000
-               print(delay)
-               page.wait_for_timeout(delay)
-               page.mouse.move(x=500, y=400)
-               page.wait_for_load_state('networkidle')
-               page.mouse.wheel(delta_y=2000, delta_x=0)
-               page.wait_for_load_state('networkidle')
-               page.wait_for_timeout(5000)
-               filename = group + '-' + striptld(host['slug']) + '.html'
-               name = os.path.join(os.getcwd(), 'source', filename)
-               with open(name, 'w', encoding='utf-8') as sitesource:
+                browser = play.chromium.launch(proxy={"server": "socks5://127.0.0.1:9050"},
+                          args=['--unsafely-treat-insecure-origin-as-secure='+host['slug']])
+                context = browser.new_context(ignore_https_errors= True )
+                page = context.new_page()
+                if 'timeout' in host and host['timeout'] is not None:
+                    page.goto(host['slug'], wait_until='load', timeout = host['timeout']*1000)
+                else:
+                    page.goto(host['slug'], wait_until='load', timeout = 120000)
+                page.bring_to_front()
+                delay = host['delay']*1000 if ( 'delay' in host and host['delay'] is not None ) \
+                    else 15000
+                page.wait_for_timeout(delay)
+                page.mouse.move(x=500, y=400)
+                page.wait_for_load_state('networkidle')
+                page.mouse.wheel(delta_y=2000, delta_x=0)
+                page.wait_for_load_state('networkidle')
+                page.wait_for_timeout(5000)
+                filename = group + '-' + striptld(host['slug']) + '.html'
+                name = os.path.join(os.getcwd(), 'source', filename)
+                with open(name, 'w', encoding='utf-8') as sitesource:
                     sitesource.write(page.content())
                     sitesource.close()
 
-               filename = group + '-' + createfile(host['slug']) + '.png'
-               name = os.path.join(os.getcwd(), 'docs/screenshots', filename)
-               saved = page.screenshot(path=name, full_page=True)
-               lock.acquire()
-               host['available'] = True
-               host['title'] = page.title()
-               host['lastscrape'] = str(datetime.today())
-               host['updated'] = str(datetime.today())
-               lock.release()
+                filename = group + '-' + createfile(host['slug']) + '.png'
+                name = os.path.join(os.getcwd(), 'docs/screenshots', filename)
+                page.screenshot(path=name, full_page=True)
+                lock.acquire()
+                host['available'] = True
+                host['title'] = page.title()
+                host['lastscrape'] = str(datetime.today())
+                host['updated'] = str(datetime.today())
+                lock.release()
             except PlaywrightTimeoutError:
-               stdlog('Timeout!')
-            except Exception as e:
-               errlog(e)
-               errlog("error")
+                stdlog('Timeout!')
+            except Exception as exception:
+                errlog(exception)
+                errlog("error")
             browser.close()
             stdlog('leaving : ' + host['fqdn']+ ' --------- ' + group)
-            q.task_done()
-
+            queuethread.task_done()
 
 def scraper(file: str) -> None:
     '''main scraping function'''
     groups = openjson(file)
     lock = Lock()
-    q = queue.Queue() # type: ignore
-    for numthread in range(get_config('generic','thread')):
-        t1 = Thread(target=threadscape, args=(q,lock), daemon=True)
-        t1.start()
+    queuethread = queue.Queue() # type: ignore
+    for _ in range(get_config('generic','thread')):
+        thread1 = Thread(target=threadscape, args=(queuethread,lock), daemon=True)
+        thread1.start()
     for group in groups:
         stdlog('ransomloook: ' + 'working on ' + group['name'])
         # iterate each location/mirror/relay
         for host in group['locations']:
             data=[host,group['name']]
-            q.put(data)
-    q.join()
+            queuethread.put(data)
+    queuethread.join()
     time.sleep(5)
     stdlog('Writing result')
     with open(file, 'w', encoding='utf-8') as groupsfile:
@@ -128,7 +127,8 @@ def adder(name: str, location: str) -> None:
     handles the addition of new providers to groups.json
     '''
     if checkexisting(name):
-        stdlog('ransomlook: ' + 'records for ' + name + ' already exist, appending to avoid duplication')
+        stdlog('ransomlook: ' + 'records for ' + name + \
+            ' already exist, appending to avoid duplication')
         appender(name, location)
     else:
         groups = openjson("data/groups.json")
@@ -148,9 +148,9 @@ def appender(name: str, location: str) -> None:
     for group in groups:
         if group['name'] == name:
             for loc in group['locations']:
-                 if location == loc['slug']:
-                     errlog('cannot append to non-existing provider or the location already exists')
-                     return
+                if location == loc['slug']:
+                    errlog('cannot append to non-existing provider or the location already exists')
+                    return
             group['locations'].append(siteschema(location))
             success = True
     if success:
