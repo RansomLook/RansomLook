afb89aed631638e453c0504ebf9804b649a42f6a
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-09-10 08:12:19 +0200

    handle exception in rocket module

--- ransomlook/rocket.py
+++ ransomlook/rocket.py
@@ -4,11 +4,15 @@
 RocketChat module
 '''
 from rocketchat_API.rocketchat import RocketChat # type: ignore
+from .sharedutils import errlog
 
 def rocketnotify(config, group, post) -> None :
     '''
     Posting message to RocketChat
     '''
-    rocket = RocketChat(user_id=config['user_id'], auth_token=config['auth_token'], \
-        server_url=config['server'], ssl_verify=config['ssl_verify'])
-    rocket.chat_post_message('New post from '+group+' : '+post, room_id=config['channel_name'])
+    try:
+        rocket = RocketChat(user_id=config['user_id'], auth_token=config['auth_token'], \
+            server_url=config['server'], ssl_verify=config['ssl_verify'])
+        rocket.chat_post_message('New post from '+group+' : '+post, room_id=config['channel_name'])
+    except:
+        errlog('Can not connect to Rocket')
