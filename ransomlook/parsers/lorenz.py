502a03a42aa98db80e63fd7f1b7faf244e58e468
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-09-17 11:04:11 +0200

    fixing lorenz parser

--- ransomlook/parsers/lorenz.py
+++ ransomlook/parsers/lorenz.py
@@ -9,10 +9,10 @@ def main():
             html_doc='source/'+filename
             file=open(html_doc,'r')
             soup=BeautifulSoup(file,'html.parser')
-            divs_name=soup.find_all('div', {"class": "col-sm-3 sidenav"})
+            divs_name=soup.find_all('div', {"class": "panel-heading"})
             for div in divs_name:
-                for item in div.find_all('li') :
-                    list_div.append(item.a.text.strip())
+                for item in div.find_all('h3') :
+                    list_div.append(item.text.strip())
             file.close()
     list_div = list(dict.fromkeys(list_div))
     print(list_div)
