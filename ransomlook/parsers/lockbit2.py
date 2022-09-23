3916cee133c36ce104f59eeb3109b775cc9d94ea
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-09-23 11:39:53 +0200

    adding description for lockbit and vicesociety parsers

--- ransomlook/parsers/lockbit2.py
+++ ransomlook/parsers/lockbit2.py
@@ -5,14 +5,19 @@ def main():
     list_div=[]
 
     for filename in os.listdir('source'):
-        if filename.startswith(__name__.split('.')[-1]+'-'):
-            html_doc='source/'+filename
-            file=open(html_doc,'r')
-            soup=BeautifulSoup(file,'html.parser')
-            divs_name=soup.find_all('div', {"class": "post-title"})
-            for div in divs_name:
-                for item in div.contents :
-                    list_div.append(item.text.strip())
-            file.close()
-    list_div = list(dict.fromkeys(list_div))
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "post-block bad"})
+                for div in divs_name:
+                    title = div.find('div',{"class": "post-title"}).text.strip()
+                    description = div.find('div',{"class" : "post-block-text"}).text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
     return list_div
--- ransomlook/parsers/vicesociety.py
+++ ransomlook/parsers/vicesociety.py
@@ -5,15 +5,21 @@ def main():
     list_div=[]
 
     for filename in os.listdir('source'):
-        if filename.startswith(__name__.split('.')[-1]+'-'):
-            html_doc='source/'+filename
-            file=open(html_doc,'r')
-            soup=BeautifulSoup(file,'html.parser')
-            divs_name=soup.find_all('font',{'size':'4', 'color':'#FFFFFF'})
-            for div in divs_name:
-                for item in div.contents :
-                    list_div.append(item.text.strip())
-            file.close()
-    list_div = list(dict.fromkeys(list_div))
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('td',{"valign":"top"})
+                for div in divs_name:
+                    title = div.find("font", {"size":4}).text.strip()
+                    for description in div.find_all("font", {"size":2, "color":"#5B61F6"}):
+                        if not description.b.text.strip().startswith("http"):
+                            list_div.append({"title" : title, "description" : description.b.text.strip()})
+                            break
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
     print(list_div)
     return list_div
