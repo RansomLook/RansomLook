3a099d2167d39dad77e4462f2f02a04d7742832b
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-09-24 14:49:05 +0200

    parsers

--- ransomlook/parsers/0mega.py
+++ ransomlook/parsers/0mega.py
@@ -5,15 +5,20 @@ def main():
     list_div=[]
 
     for filename in os.listdir('source'):
-        if filename.startswith(__name__.split('.')[-1]+'-'):
-            html_doc='source/'+filename
-            file=open(html_doc,'r')
-            soup=BeautifulSoup(file,'html.parser')
-            divs_name=soup.find_all('tr', {"class": "trow"})
-            for div in divs_name:
-                item = div.find_all('td')
-                list_div.append(item[0].text.strip())
-            file.close()
-    list_div = list(dict.fromkeys(list_div))
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('tr', {"class": "trow"})
+                for div in divs_name:
+                    item = div.find_all('td')
+                    title = item[0].text.strip()
+                    description = item[2].text.strip()
+                    list_div.append({'title':title, 'description': description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
     print(list_div)
     return list_div
--- ransomlook/parsers/bianlian.py
+++ ransomlook/parsers/bianlian.py
@@ -5,15 +5,19 @@ def main():
     list_div=[]
 
     for filename in os.listdir('source'):
-        if filename.startswith(__name__.split('.')[-1]+'-'):
-            html_doc='source/'+filename
-            file=open(html_doc,'r')
-            soup=BeautifulSoup(file,'html.parser')
-            divs_name=soup.find_all('h1', {"class": "title"})
-            for div in divs_name:
-                for item in div.contents :
-                    list_div.append(item.text.strip())
-            file.close()
-    list_div = list(dict.fromkeys(list_div))
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('section', {"class": "list-item"})
+                for div in divs_name:
+                    title = div.h1.text.strip()
+                    description = div.div.text.strip()
+                    list_div.append({'title':title, 'description': description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
     print(list_div)
     return list_div
