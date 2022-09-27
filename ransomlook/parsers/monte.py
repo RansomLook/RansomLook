123ed0e6e27482b538c08c6002823d39c75d8ebe
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-09-27 10:32:28 +0200

    monte parser

---
+++ ransomlook/parsers/monte.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import BeautifulSoup # type: ignore
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "blog-card p-3 col-md-9"})
+                for div in divs_name:
+                    title = div.find('div', {'class': 'post-header col-md-12 no-pad px-3'}).a.text.strip()
+                    description =  div.find('div', {'class': 'post-short-description col-md-12 no-pad px-3 mt-5'}).p.text.strip()
+                    list_div.append({'title':title, 'description': description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
