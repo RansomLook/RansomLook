bc3564c913820b0bdc0a6f11350fb40b2a0bad21
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-11-01 13:57:38 +0100

    adding mallox parser

---
+++ ransomlook/parsers/mallox.py
@@ -0,0 +1,25 @@
+import os
+from bs4 import BeautifulSoup # type: ignore
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "card mb-4 box-shadow"})
+                for div in divs_name:
+                    title = div.find('h5',{"class": "card-title"}).text.strip()
+                    description = ''
+                    for p in div.find_all('p'):
+                        description+=p.text+'\n'
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
