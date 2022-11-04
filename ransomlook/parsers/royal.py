158b125895b8f8b4c0c95d8e0aee87ad858c1801
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-11-04 15:28:33 +0100

    adding royal parser

---
+++ ransomlook/parsers/royal.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import BeautifulSoup # type: ignore
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "card"})
+                for div in divs_name:
+                    title = div.find('h2').text.strip()
+                    description = div.find('main').text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
