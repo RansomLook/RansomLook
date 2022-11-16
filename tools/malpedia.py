d2661a5806a3861f98fc0c34456f1172c0ce4575
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-11-16 10:34:46 +0100

    Fixing malpedia import. Adding check on API key

--- tools/malpedia.py
+++ tools/malpedia.py
@@ -5,12 +5,20 @@ import requests
 
 from ransomlook.default import get_socket_path, get_config
 
+
+
 red = redis.Redis(unix_socket_path=get_socket_path('cache'), db=0)
 
 malpedia = get_config('generic','malpedia')
-print(malpedia)
+if malpedia == "":
+    print("No APIKEY for Malpedia")
+    exit(0)
 
 response =  requests.get('https://malpedia.caad.fkie.fraunhofer.de/api/get/families', headers={'Authorization': 'apitoken '+malpedia})
+if response.status_code != 200:
+    print(response.text)
+    exit(0)
+
 families = json.loads(response.text)
 
 keys = red.keys()
