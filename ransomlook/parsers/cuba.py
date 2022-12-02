1316d495d761fd81ba20f46b8aa22b172b345c25
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2022-12-02 10:43:27 +0100

    Update cuba.py

--- ransomlook/parsers/cuba.py
+++ ransomlook/parsers/cuba.py
@@ -12,8 +12,8 @@ def main():
                 soup=BeautifulSoup(file,'html.parser')
                 divs_name=soup.find_all('div', {'class':'list-text'})
                 for div in divs_name:
-                    title = div.a['href'].split('/')[3]
-                    description = div.a.p.text.strip()
+                    title = div.a['href'].split('/')[4]
+                    description = div.a.text.strip()
                     list_div.append({'title':title, 'description': description})
                 file.close()
         except:
