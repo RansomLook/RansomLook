effa645e9c93bde9bb14dd9ea9016dc178535183
Fafner [_KeyZee_] <fkz@p4nd4.land>
2022-12-23 12:35:37 +0100

    Adding nokoyawa parser

---
+++ ransomlook/parsers/nokoyawa.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import BeautifulSoup # type: ignore
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "relative bg-white rounded-lg shadow dark:bg-gray-700"})
+                for div in divs_name:
+                    title = div.find('h3').text.strip().split('\n')[0].strip()
+                    description = div.find('p', {'class':"break-all"}).text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
