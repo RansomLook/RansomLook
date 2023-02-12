82d044fc7fbc9da4c0b2f9e585721505f35c3982
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-02-12 22:32:34 +0100

    adding V is Vendetta parser

---
+++ ransomlook/parsers/v is vendetta.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "post"})
+                for div in divs_name:
+                    title = div.a['href'].split('/')[2]
+                    description = div.find('p').text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
