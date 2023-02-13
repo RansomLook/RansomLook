17dcad9f9cf1e358504979deb66d48f3857a4a76
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-02-13 14:23:32 +0100

    Medusa parser

---
+++ ransomlook/parsers/medusa.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "card"})
+                for div in divs_name:
+                    title = div.find('h3', {"class":"card-title"}).text
+                    description = div.find("div", {"class": "card-body"}).text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
