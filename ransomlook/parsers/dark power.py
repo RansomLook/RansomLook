bf9cf1c15042609c62185101b766d5936d5a16f4
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-03-09 15:00:19 +0100

    Dark Power parser

---
+++ ransomlook/parsers/dark power.py
@@ -0,0 +1,24 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "sm:w-1/2 mb-10 px-4"})
+                for div in divs_name:
+                    print (div)
+                    title = div.find('h2').text.strip()
+                    description = ''
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
