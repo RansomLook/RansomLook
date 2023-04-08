a1369e82dbe7cbaadcda04ca3698f580ffa4686d
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-04-08 16:38:04 +0200

    fix

--- ransomlook/rocket.py
+++ ransomlook/rocket.py
@@ -29,7 +29,7 @@ def rocketnotifyrf(config, datas) -> None :
     try:
         rocket = RocketChat(user_id=config['user_id'], auth_token=config['auth_token'], \
             server_url=config['server'], ssl_verify=config['ssl_verify'])
-        rocket.chat_post_message('New Recorded Future Dump ' + datas['name'] + " : " + datas['description']), room_id=config['channel_name'])
+        rocket.chat_post_message('New Recorded Future Dump ' + datas['name'] + " : " + datas['description'], room_id=config['channel_name'])
     except:
         errlog('Can not connect to Rocket')
 
