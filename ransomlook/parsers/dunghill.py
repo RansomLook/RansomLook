25d2c700b21c0cda9400f43ac6c44ac68cc58abd
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-04-11 19:32:28 +0200

    Dunghill parser

---
+++ ransomlook/parsers/dunghill.py
@@ -0,0 +1,22 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                title = soup.find('div', {"class": "block-heading pt-4 mt-5"}).text.strip()
+                print(title)
+                description = soup.find("div", {"class": ""}).text.strip()
+                list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
