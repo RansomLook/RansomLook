85ee25b07ac28dbac79199b5725bb2682edc4a26
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-04-17 15:09:21 +0200

    CrossLock parser

---
+++ ransomlook/parsers/crosslock.py
@@ -0,0 +1,26 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "blog-posts"})
+                for div in divs_name:
+                    title = div.find('h2').text.strip()
+                    descriptions = div.find_all('p')
+                    description=''
+                    for p in descriptions:
+                        description +=p.text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
