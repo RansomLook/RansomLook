298cc7f144bdade9de801fe9904a455a09c30376
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-04-30 19:52:19 +0200

    fix qilin parser

--- ransomlook/parsers/qilin.py
+++ ransomlook/parsers/qilin.py
@@ -10,10 +10,10 @@ def main():
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
                 soup=BeautifulSoup(file,'html.parser')
-                divs_name=soup.find_all('div', {"class": "MainCard_card__2aCY4"})
+                divs_name=soup.find_all('div', {"class": "item_box"})
                 for div in divs_name:
-                    title = div.find('h4',{"class": "MainCard_card__title__jdBK+"}).text.strip()
-                    description = div.find('div',{"class": "MainCard_card__body_column__rZHSt"}).find('p',{"class" : "MainCard_card__body_text__4AtzB"}).text.strip()
+                    title = div.find('a',{"class": "item_box-title mb-2 mt-1"}).text.strip()
+                    description = div.find('div',{"class": "item_box_text"}).text.strip()
                     list_div.append({"title" : title, "description" : description})
                 file.close()
         except:
