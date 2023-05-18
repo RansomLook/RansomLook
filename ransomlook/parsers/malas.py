82d5de447ef6b282fd70359cdd12e0823acac23d
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-05-18 11:19:53 +0200

    Fix malas parser & make mypy happy

--- ransomlook/parsers/malas.py
+++ ransomlook/parsers/malas.py
@@ -1,3 +1,4 @@
+
 import os
 from bs4 import BeautifulSoup
 
@@ -10,15 +11,23 @@ def main():
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
                 soup=BeautifulSoup(file,'html.parser')
-                list = soup.find('ul', {"class":"list"})
-                divs_name=list.find_all('li')
-                for div in divs_name:
-                    title = div.a['title'].strip()
-                    description = ""
-                    list_div.append({"title" : title, "description" : description})
+                if filename.endswith('xml.html'):
+                    items = soup.find_all('item')
+                    for item in items:
+                        title = item.title.text
+                        description =  item.description.text
+                        list_div.append({"title" : title, "description" : description})
+                else:
+                    liste = soup.find('ul', {"class":"list"} )
+                    divs_name=liste.find_all('li') # type: ignore
+                    for div in divs_name:
+                        title = div.a['title'].strip()
+                        description = ""
+                        list_div.append({"title" : title, "description" : description})
                 file.close()
         except:
             print("Failed during : " + filename)
             pass
     print(list_div)
+    list_div = []
     return list_div
