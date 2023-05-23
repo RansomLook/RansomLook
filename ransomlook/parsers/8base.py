559b25afe62d543e9e7263f7b124f3ea9a4f72c2
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-05-23 16:04:32 +0200

    8base parser

---
+++ ransomlook/parsers/8base.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "list-group-item"})
+                for div in divs_name:
+                    title = div.find('a').text.split(':')[0].strip()
+                    description = div.find('div',{"class" : "small opacity-50"}).text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
