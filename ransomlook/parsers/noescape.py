e5fa80c51bd424983b9caa691a2e2ba405a75452
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-06-16 11:46:53 +0200

    Fix noescape parser

--- ransomlook/parsers/noescape.py
+++ ransomlook/parsers/noescape.py
@@ -10,10 +10,15 @@ def main():
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
                 soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "bg-cover border-rounded mb-3"})
+                for div in divs_name:
+                    title = div.find('h2', {"title": "Company name"}).text.strip()
+                    description = div.find('div',{"class" : "fs-5"}).text.strip()
+                    list_div.append({"title" : title, "description" : description})
                 divs_name=soup.find_all('div', {"class": "col-lg-4 my-3 d-flex flex-column justify-content-between"})
                 for div in divs_name:
-                    title = div.find('a', {"class": "h2 fs-4 text-decoration-none d-block text-center text-nowrap overflow-hidden"}).text.strip()
-                    description = div.find('p',{"class" : "mb-2 text-justify"}).text.strip()
+                    title = div.find('h2', {"title": "Company name"}).text.strip()
+                    description = div.find('div',{"class" : "mb-2 text-justify"}).text.strip()
                     list_div.append({"title" : title, "description" : description})
                 file.close()
         except:
