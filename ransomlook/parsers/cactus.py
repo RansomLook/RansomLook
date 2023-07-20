719b1c0caacca819fc9b15e1f8691c80f75df795
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-07-20 13:24:50 +0000

    cactus parser

---
+++ ransomlook/parsers/cactus.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('article')
+                for div in divs_name:
+                    title = div.find('a').text.strip()
+                    description = div.find('p').text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
