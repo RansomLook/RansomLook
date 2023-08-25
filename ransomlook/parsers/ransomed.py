a5114fa9df2d137096d54178a29c300b7dfc223a
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-08-25 06:59:01 +0000

    Fix ransomed for new website

--- ransomlook/parsers/ransomed.py
+++ ransomlook/parsers/ransomed.py
@@ -4,17 +4,19 @@ from bs4 import BeautifulSoup
 def main():
     list_div=[]
     for filename in os.listdir('source'):
-        if filename.startswith(__name__.split('.')[-1]+'-'):
-            html_doc='source/'+filename
-            print(filename)
-            file=open(html_doc,'r')
-            soup=BeautifulSoup(file,'html.parser')
-            div = soup.find('div',{"class":"di"})
-            divs_name=div.find_all('a') # type: ignore
-            for a in divs_name:
-                if a.text.strip() != "{Censored}":
-                    list_div.append(a.text.strip().replace('{','').replace('}',''))
-            file.close()
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                print(filename)
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                div = soup.find('div')
+                divs_name=div.find_all('a', {"style":"margin-bottom: 10px; color: #ccc; font-family: monospace; text-decoration: underline; cursor: pointer;"}) # type: ignore
+                for a in divs_name:
+                    list_div.append(a.text.strip())
+                file.close()
+        except:
+            pass
     print(list_div)
     list_div = list(dict.fromkeys(list_div))
     return list_div
