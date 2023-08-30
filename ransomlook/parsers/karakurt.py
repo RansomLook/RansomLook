4d681b5a9c77a58ca74ea019d45d0ed196db5541
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-08-30 15:47:08 +0000

    Fix karakurt parser and Inc ransom

--- ransomlook/parsers/karakurt.py
+++ ransomlook/parsers/karakurt.py
@@ -10,18 +10,30 @@ def main():
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
                 soup=BeautifulSoup(file,'html.parser')
-                divs_name=soup.find_all('article', {"class": "ciz-post"})
+                prereleases = soup.find('div', {"id": "companies_prereleases"})
+                divs_name = prereleases.find_all('div', {"class": "col-md-4 col-sm-4 col-xs-12"})
+                for div in divs_name:
+                    h3 = div.find('h3')
+                    title = h3.text.strip()
+                    description = div.find('div', {'class': 'post-des'}).p.text.strip()
+                    link = h3.a['href']
+                    list_div.append({'title':title, 'description': description, 'link': link, 'slug': filename})
+                divs_name=soup.find_all('div', {"class": "col-xs-6 col-md-3 col-sm-3"})
                 for div in divs_name:
                     title = div.h3.a.text.strip()
-                    description = div.find('div', {'class': 'post-des'}).p.text.strip()
-                    list_div.append({'title':title, 'description': description})
+                    description = div.find('div', {'class': 'post-des'}).text.strip()
+                    print(description)
+                    link = div.h3.a['href']
+                    list_div.append({'title':title, 'description': description, 'link': link, 'slug': filename})
                 divs_name=soup.find_all('div', {"class": "category-mid-post-two"})
                 for div in divs_name:
                     title = div.h2.a.text.strip()
                     description = div.find('div', {'class': 'post-des dropcap'}).p.text.strip()
-                    list_div.append({'title':title, 'description': description})
+                    link = div.h2.a['href']
+                    list_div.append({'title':title, 'description': description, 'link': link, 'slug': filename})
                 file.close()
         except:
+            
             print("Failed during : " + filename)
             pass
     print(list_div)
