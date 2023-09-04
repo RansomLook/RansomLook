e8394672fc1042034b712ca5ebc1f6d822490c7a
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-09-04 15:26:49 +0000

    Fixing alpv API parser

--- ransomlook/parsers/alphv.py
+++ ransomlook/parsers/alphv.py
@@ -16,7 +16,8 @@ def main():
                     for entry in data['items']:
                         title = entry['title'].strip()
                         description = entry['publication']['description'].strip()
-                        list_div.append({'title':title, 'description': description})
+                        link = entry['id']
+                        list_div.append({'title':title, 'description': description, 'link': link, 'slug': filename})
                 else :
                     divs_name=soup.find_all('div', {'class': 'post'})
                     for div in divs_name:
