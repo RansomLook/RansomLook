4bb491f859e2d3507cbebed8a07f93543cabaa06
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-09-18 20:29:36 +0000

    fix noescape

--- ransomlook/parsers/noescape.py
+++ ransomlook/parsers/noescape.py
@@ -21,6 +21,12 @@ def main():
                     description = div.find('div',{"class" : "mb-2 text-justify"}).text.strip()
                     link = div.find('div',{"class": "d-flex align-items-center justify-content-between"}).a["href"]
                     list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+                divs_name=soup.find_all('div', {"class": "d-flex flex-column justify-content-between flex-fill"})
+                for div in divs_name:
+                    title = div.find('h2', {"title": "Company name"}).text.strip()
+                    description = div.find('small',{"class" : "text-justify"}).text.strip()
+                    link = div.find('a',{"class": "btn btn-sm btn-primary h2 mb-0"})["href"]
+                    list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
                 file.close()
         except:
             print("Failed during : " + filename)
