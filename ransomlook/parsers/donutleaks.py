36e254208b1736174fc39ec012062f44f65a7cdd
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-09-30 06:43:29 +0000

    Fix donutleaks parser

--- ransomlook/parsers/donutleaks.py
+++ ransomlook/parsers/donutleaks.py
@@ -14,7 +14,8 @@ def main():
                 for div in divs_name:
                     title = div.find('h2').text.strip()
                     description = div.find('p').text.strip()
-                    list_div.append({'title':title, 'description': description})
+                    link = div.find('h2').a['href']
+                    list_div.append({'title':title, 'description': description, 'link': link, 'slug': filename})
                 file.close()
         except:
             print("Failed during : " + filename)
