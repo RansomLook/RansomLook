db90d57c985ca4db72034e01774aea416d980ada
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-10-30 20:43:58 +0000

    Fix knight

--- ransomlook/parsers/knight.py
+++ ransomlook/parsers/knight.py
@@ -17,6 +17,17 @@ def main():
                     description = div.find("p").text.strip()
                     link = a['href']
                     list_div.append({"title" : title, "description" : description, 'link': link, 'slug': filename})
+                divs_name=soup.find_all('div', {"class": "card-body"})
+                for div in divs_name:
+                    try: 
+                        h2 = div.find('h2',{"class":"card-title"})
+                        title = h2.text.strip()
+                        description = div.find("p").text.strip()
+                        link = h2.a['href']
+                        list_div.append({"title" : title, "description" : description, 'link': link, 'slug': filename})
+                    except:
+                        pass
+
                 file.close()
         except:
             print("Failed during : " + filename)
