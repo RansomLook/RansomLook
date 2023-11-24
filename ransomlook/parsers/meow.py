18b5926502f3eb7b246bbd4a952c64b2ed3187f1
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2023-11-24 08:43:46 +0100

    Create meow.py
    Adding Meow Ransomware

---
+++ ransomlook/parsers/meow.py
@@ -0,0 +1,24 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation1 MuiCard-root story-card css-76n6mc"})
+                for div in divs_name:
+                    title = div.find('div', {"class": "MuiTypography-root MuiTypography-h5 MuiTypography-gutterBottom css-bp7fp2"}).text.strip()
+                    description = div.find('p',{"class":"MuiTypography-root MuiTypography-body2 css-1nwimy0"}).text.strip()
+                    link = div.find('a')["href"]
+                    list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
