b1396e71712f857d3b48144ddfef8eb86fecbf6d
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2023-12-09 10:32:22 +0100

    Create siegedsec.py

---
+++ ransomlook/parsers/siegedsec.py
@@ -0,0 +1,27 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('tr')
+                for div in divs_name:
+                    item = div.find_all('td')
+                    if item == []:
+                       continue
+                    title = item[0].text.strip()
+                    description = item[2].text.strip()
+                    link = item[5].a['href']
+                    list_div.append({'title':title, 'description': description, 'link' : link, 'slug' :  filename})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
