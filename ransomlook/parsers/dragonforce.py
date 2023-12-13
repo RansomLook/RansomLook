259aac660c0dda6c2e3cfde76e631e7ee8406a09
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-12-13 15:31:03 +0000

    DragonForce parser

---
+++ ransomlook/parsers/dragonforce.py
@@ -0,0 +1,22 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+    for filename in os.listdir('source'):
+        if filename.startswith(__name__.split('.')[-1]+'-'):
+            try:
+                html_doc='source/'+filename
+                print(filename)
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name = soup.find_all('div',{"class":"publications-list__publication"})
+                for div in divs_name:
+                    title = div.find('h3',{"class":"list-publication__name"}).text.strip()
+                    description = div.find('p',{"class":"list-publication__description"}).text.strip()
+                    list_div.append({"title": title, "description": description})
+                file.close()
+            except:
+                pass
+    print(list_div)
+    return list_div
