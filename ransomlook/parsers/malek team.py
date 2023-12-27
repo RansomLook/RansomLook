91bb395026cdfc04fd33c4a99731d2dde742ac54
Fafner [_KeyZee_] <fkz@p4nd4.land>
2023-12-27 13:48:50 +0000

    Malek Team parser

---
+++ ransomlook/parsers/malek team.py
@@ -0,0 +1,27 @@
+import os
+from bs4 import BeautifulSoup
+
+def main():
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "timeline_item"})
+                for div in divs_name:
+                    title = div.find('div', {"class":"timeline_date-text"}).text
+                    description = div.find("div", {"class": "margin-bottom-medium"}).text.strip()
+                    try:
+                        link = div.find('a', {"class":"btn btn-danger"})['href']
+                        list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+                    except:
+                        list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
