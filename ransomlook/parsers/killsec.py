e81cf759b9d9f47a408cd1e926ee4c5f1029405c
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-03-21 18:56:27 +0000

    killsec parser

---
+++ ransomlook/parsers/killsec.py
@@ -0,0 +1,31 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('a', {"class": "post-block unleaked"})
+                for div in divs_name:
+                    title = div.find('div',{"class": "post-title"}).text.strip()
+                    description = div.find('p',{"class" : "post-block-text"}).text.strip()
+                    link = div['href']
+                    list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+                divs_name=soup.find_all('a', {"class": "post-block leaked"})
+                for div in divs_name:
+                    title = div.find('div',{"class": "post-title"}).text.strip()
+                    description = div.find('p',{"class" : "post-block-text"}).text.strip()
+                    link = div['href']
+                    list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
