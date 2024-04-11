99b1c44fcb11ed873fa9e4de211de15fedcafe23
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-04-11 11:25:27 +0000

    Fix Mallox

--- ransomlook/parsers/mallox.py
+++ ransomlook/parsers/mallox.py
@@ -23,6 +23,17 @@ def main() -> List[Dict[str, str]] :
                         list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
                     else:
                         list_div.append({"title" : title, "description" : description})
+                divs_name=soup.find_all('div', {"class": "card-body p-6 py-2"})
+                for div in divs_name:
+                    data=div.find_all('div')
+                    title=data[0].text.strip()
+                    description=data[1].text.strip()
+                    linkdata=data[2].find('a')
+                    if linkdata == None:
+                        list_div.append({"title" : title, "description" : description})
+                    else:
+                        link = linkdata['href']
+                        list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
                 file.close()
         except:
             print("Failed during : " + filename)
