daca5979eb8e8c2edc111d6beb18ab799b5a3daf
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-06-04 09:52:08 +0000

    Fix ransomhub

--- ransomlook/parsers/ransomhub.py
+++ ransomlook/parsers/ransomhub.py
@@ -13,9 +13,21 @@ def main() -> List[Dict[str, str]] :
                 soup=BeautifulSoup(file,'html.parser')
                 divs_name=soup.find_all('div', {"class": "card-body"})
                 for div in divs_name:
-                    title = div.find('a').text.strip()
-                    description = div.find('p',{"class" : "card-text"}).text.strip()
-                    list_div.append({"title" : title, "description" : '', "link" : div.find('a')['href'], "slug": filename})
+                    try :
+                        title = div.find('a').text.strip()
+                        description = div.find('p',{"class" : "card-text"}).text.strip()
+                        list_div.append({"title" : title, "description" : '', "link" : div.find('a')['href'], "slug": filename})
+                    except:
+                        pass
+                divs_name=soup.find_all('div', {"class": "col-12 col-md-6 col-lg-4"})
+                for div in divs_name:
+                    try :
+                        title = div.find('div', {"class":"card-title text-center"}).text.strip()
+                        description = ""
+                        list_div.append({"title" : title, "description" : '', "link" : div.find('a')['href'], "slug": filename})
+                    except:
+                        pass
+
                 file.close()
         except:
             print("Failed during : " + filename)
