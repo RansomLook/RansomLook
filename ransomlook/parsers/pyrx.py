3dc638f282abdbe805c84889fc991126aa5e54b8
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-07-03 06:09:35 +0000

    Pyrx parser

---
+++ ransomlook/parsers/pyrx.py
@@ -0,0 +1,27 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('td')
+                for div in divs_name:
+                    title = div.a.text.replace('[*]','').strip()
+                    if title.lower() in ['soon','contact information','public pgp key','all updates','breaches and operations by pryx.']:
+                        continue
+                    description = ''
+                    link = div.a['href']
+                    list_div.append({'title':title, 'description': description, 'link' : link, 'slug' :  filename})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
