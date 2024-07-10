12d45ac293f9891a5054fca4dbca07ae48a4406f
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-07-10 12:38:39 +0000

    Vanir Group Parser

---
+++ ransomlook/parsers/vanir group.py
@@ -0,0 +1,28 @@
+import os
+from typing import Dict, List
+import re
+import json
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+    for filename in os.listdir('source'):
+        if filename.startswith(__name__.split('.')[-1]+'-'):
+            html_doc='source/'+filename
+            file=open(html_doc,'r')
+            if '.js' in filename:
+                content = file.read()
+                matches = re.search('projects:(.*)}}},P', content, re.IGNORECASE)
+                myjson= matches.group(1).replace("projectName:", '"projectName":')\
+                         .replace("projectDescription:", '"projectDescription":')\
+                         .replace("githubLink:", '"githubLink":')\
+                         .replace("websiteLink:", '"websiteLink":')\
+                         .replace("tags:", '"tags":')
+
+                data = json.loads(myjson) # type: ignore
+                for entry in data:
+                    title = entry['projectName'].strip()
+                    description = entry['projectDescription'].strip()
+                    list_div.append({"title" : title, "description" : description})
+            file.close()
+    print(list_div)
+    return list_div
