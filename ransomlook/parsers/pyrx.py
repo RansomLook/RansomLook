03833d7ff23a2ae50e4bac38765a014fadebe0ff
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-09-14 08:56:36 +0000

    Fix Pyrx parser

--- ransomlook/parsers/pyrx.py
+++ ransomlook/parsers/pyrx.py
@@ -19,6 +19,12 @@ def main() -> List[Dict[str, str]] :
                     description = ''
                     link = div.a['href']
                     list_div.append({'title':title, 'description': description, 'link' : link, 'slug' :  filename})
+                divs_name=soup.find_all('div', {'class':'update-box'})
+                for div in divs_name:
+                    title = div.find('h3').text.strip()
+                    description = div.find('p').text.strip()
+                    link = div.find('a')['href']
+                    list_div.append({'title':title, 'description': description, 'link' : link, 'slug' :  filename})
                 file.close()
         except:
             print("Failed during : " + filename)
