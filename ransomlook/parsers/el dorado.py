dbb682088fc8e561ce9df7f675366fc242684a0d
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-09-17 20:03:52 +0000

    Update El Dorado parser

--- ransomlook/parsers/el dorado.py
+++ ransomlook/parsers/el dorado.py
@@ -21,6 +21,25 @@ def main() -> List[Dict[str, str]] :
                         pass
                     link = div.find('a')['href']
                     list_div.append({"title" : title, "description" : description, 'link': link, 'slug': filename})
+                divs_name=soup.find_all('div')
+                for div in divs_name:
+                    try: 
+                        print(div)
+                        try:
+                            title = div.find('h3').text.strip()
+                        except:
+                            title = div.find('h2').text.strip()
+                        try:
+                            description=""
+                            descs = div.find_all('p')
+                            for desc in descs:
+                                description +=desc.text.strip()+' '
+                        except:
+                            description = ''
+                            pass
+                        list_div.append({"title" : title, "description" : description})
+                    except:
+                        pass
                 file.close()
         except:
             print("Failed during : " + filename)
