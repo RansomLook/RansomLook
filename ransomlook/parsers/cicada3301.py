0293488f022c130601bc27a653f5066b0059c763
VicoLeChips <62722776+VicoLeChips@users.noreply.github.com>
2024-09-26 14:42:02 +1000

    Update cicada3301.py Parser
    Fix: Explicitly setting file encoding to UTF-8 + link exception issue

--- ransomlook/parsers/cicada3301.py
+++ ransomlook/parsers/cicada3301.py
@@ -10,13 +10,16 @@ def main() -> List[Dict[str, str]] :
             if filename.startswith(__name__.split('.')[-1]+'-'):
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
-                soup=BeautifulSoup(file,'html.parser')
+                soup=BeautifulSoup(file,'html.parser',encoding='utf-8')
                 divs_name=soup.find_all('div', {"class": "block relative p-8 bg-gray-800 rounded-lg transition duration-300 ease-in-out"})
                 for div in divs_name:
                     title = div.find('h2').text.strip()
                     description = div.find('p').text.strip()
                     a = div.find_all('a')
-                    link = a[1]['href']
+                    try:
+                        link = a[1]['href']
+                    except:
+                        pass
                     list_div.append({"title" : title, "description" : description, "link" : link, "slug": filename})
                 file.close()
         except:
