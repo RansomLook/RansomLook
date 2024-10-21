f24b1c168c3edc93bd59eef4f4d561f1cf5c46f3
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2024-10-21 11:54:23 +0200

    Update killsec.py

--- ransomlook/parsers/killsec.py
+++ ransomlook/parsers/killsec.py
@@ -17,18 +17,18 @@ def main() -> List[Dict[str, str]] :
                     description = div.find('p',{"class" : "post-block-text"}).text.strip()
                     try :
                         link = div['href']
+                        list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
                     except:
-                        link = "No link"
-                    list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+                        list_div.append({"title" : title, "description" : description})
                 divs_name=soup.find_all('a', {"class": "post-block leaked"})
                 for div in divs_name:
                     title = div.find('div',{"class": "post-title"}).text.strip()
                     description = div.find('p',{"class" : "post-block-text"}).text.strip()
                     try :
                         link = div['href']
+                        list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
                     except:
-                        link = "No link"
-                    list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+                        list_div.append({"title" : title, "description" : description})
                 file.close()
         except:
             print("Failed during : " + filename)
