21eb4aab45ccf8aaac6fc20c82c86c458daba475
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-11-15 18:10:38 +0000

    Chort parser

---
+++ ransomlook/parsers/chort.py
@@ -0,0 +1,25 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "card rounded-6 h-100"})
+                for div in divs_name:
+                    title = div.find('h5', {"class":"card-title"}).text
+                    description = ''
+                    link = div.find('a')['href']
+                    list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
