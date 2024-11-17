91fc09c315c882c0af91050a316d6431128d1de2
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-11-17 20:12:44 +0000

    termite parser

---
+++ ransomlook/parsers/termite.py
@@ -0,0 +1,26 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('a', {"class": "min-h-36"})
+                for div in divs_name:
+                    title = div.find('h2').text.strip()
+                    description = div.find('h3').text.strip()
+                    link = div["href"]
+                    list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
