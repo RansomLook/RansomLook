a21ff69527b1af09407e4ecb24f7b4633af3ff19
Fafner [_KeyZee_] <fkz@p4nd4.land>
2024-12-08 11:33:01 +0000

    Fixing Funksec

--- ransomlook/parsers/funksec.py
+++ ransomlook/parsers/funksec.py
@@ -18,6 +18,13 @@ def main() -> List[Dict[str, str]] :
                     description = ''
                     link = div['href']
                     list_div.append({'title':title, 'description': description, "link": link, "slug": filename})
+                div_name=soup.find('div', {"id": "ransom"})
+                divs_name=div_name.find_all('a', {"class":"product-card"})
+                for div in divs_name:
+                    title = div.find('h2').text.strip()
+                    description = ''
+                    link = div['href']
+                    list_div.append({'title':title, 'description': description, "link": link, "slug": filename})
                 file.close()
         except:
             print("Failed during : " + filename)
