e5219966773babc99227ee2509c302a090beaed7
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-01-05 03:43:49 +0000

    Fixing Apos

--- ransomlook/parsers/apos.py
+++ ransomlook/parsers/apos.py
@@ -11,12 +11,21 @@ def main() -> List[Dict[str, str]] :
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
                 soup=BeautifulSoup(file,'html.parser')
-                header = soup.find('div',{"style":"display: grid; position: relative; grid-template-columns: repeat(auto-fill, minmax(320px, 1fr)); gap: 16px; padding-top: 16px; padding-bottom: 4px;"})
-                divs_name=header.find_all('div', {"class": "notion-selectable notion-page-block notion-collection-item"}) # type: ignore
+                try:
+                    header = soup.find('div',{"style":"display: grid; position: relative; grid-template-columns: repeat(auto-fill, minmax(320px, 1fr)); gap: 16px; padding-top: 16px; padding-bottom: 4px;"})
+                    divs_name=header.find_all('div', {"class": "notion-selectable notion-page-block notion-collection-item"}) # type: ignore
+                    for div in divs_name:
+                        title = div.find('div',{"style":"width: 100%; display: flex; padding: 8px 10px 6px; position: relative;"}).text.strip()
+                        description = div.find('div',{"style" : "padding-top: 0px; padding-bottom: 10px;"}).text.strip()
+                        list_div.append({"title" : title, "description" : description, "link" : div.a['href'], "slug": filename})
+                except:
+                    pass
+                divs_name = soup.find_all('div', {"style": "display: grid;    grid-template-columns: 200px 1fr;    grid-template-rows: auto auto auto 1fr auto;    gap: 10px;"})
                 for div in divs_name:
-                    title = div.find('div',{"style":"width: 100%; display: flex; padding: 8px 10px 6px; position: relative;"}).text.strip()
-                    description = div.find('div',{"style" : "padding-top: 0px; padding-bottom: 10px;"}).text.strip()
-                    list_div.append({"title" : title, "description" : description, "link" : div.a['href'], "slug": filename})
+                    title = div.find('h2').text.strip()
+                    description = div.find('div', {"style": "grid-column: 1 / 3;    grid-row: 3 / 4; font-size: 18px"}).text.strip()
+                    link= div.find('a')['href']
+                    list_div.append({"title" : title, "description" : description, "link" : link, "slug": filename})
                 file.close()
         except:
             print("Failed during : " + filename)
