bb400a70d2fb2982906c3d94c30f3881485c0afa
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-01-07 14:08:24 +0000

    Add Mopheus parser

---
+++ ransomlook/parsers/morpheus.py
@@ -0,0 +1,21 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        if filename.startswith(__name__.split('.')[-1]+'-'):
+            html_doc='source/'+filename
+            file=open(html_doc,'r')
+            soup=BeautifulSoup(file,'html.parser')
+            #divs_name=soup.find_all('th', {"class": "align-middle", "style":"height:63px"})
+            divs_name=soup.find_all('div', {"class": "post"})
+            for div in divs_name:
+                title = div.find('div', {"class": "post__header__title vkuiDiv vkuiRootComponent"}).text.strip()
+                description = div.find('div', {"class": "post__text parsed-post-text vkuiDiv vkuiRootComponent"}).text.strip()
+                list_div.append({"title": title, "description": description})
+            file.close()
+    print(list_div)
+    return list_div
