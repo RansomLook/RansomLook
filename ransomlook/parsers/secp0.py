93cfa07dc3dcc26bfc51bf566cd17cd65300ce6b
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-03-05 20:29:47 +0000

    Secp0 parser

---
+++ ransomlook/parsers/secp0.py
@@ -0,0 +1,24 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        if filename.startswith(__name__.split('.')[-1]+'-'):
+            html_doc='source/'+filename
+            file=open(html_doc,'r')
+            soup=BeautifulSoup(file,'html.parser')
+            divs_name=soup.find_all('div', {"class": "post"})
+            for div in divs_name:
+                title = div.find('h2').text.strip()
+                description = div.find('div',{"class":"text"}).text.strip()
+                try:
+                    link = div.find('a')['href']
+                    list_div.append({'title' : title, 'description': description, 'link': link, 'slug': filename})
+                except:
+                    list_div.append({'title' : title, 'description': description})
+            file.close()
+    print(list_div)
+    return list_div
