51e5407c1efa29f5953ca8bf87a209151f9d3721
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-03-08 17:34:27 +0000

    Fix funksec

--- ransomlook/parsers/funksec.py
+++ ransomlook/parsers/funksec.py
@@ -11,13 +11,19 @@ def main() -> List[Dict[str, str]] :
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
                 soup=BeautifulSoup(file,'html.parser')
-                div_name=soup.find('div', {"id": "breach"})
-                divs_name=div_name.find_all('a', {"class":"product-card"}) # type: ignore
-                for div in divs_name:
-                    title = div.find('h2').text.strip()
-                    description = ''
-                    link = div['href']
-                    list_div.append({'title':title, 'description': description, "link": link, "slug": filename})
+                try:
+                    div_name=soup.find_all('div', {"id": "breach"})
+                    divs_name=div_name[1].find_all('a', {"class":"product-card"}) # type: ignore
+                    for div in divs_name:
+                      try:
+                        title = div.find('h2').text.strip()
+                        description = ''
+                        link = div['href']
+                        list_div.append({'title':title, 'description': description, "link": link, "slug": filename})
+                      except:
+                        pass
+                except:
+                    pass
                 div_name=soup.find('div', {"id": "ransom"})
                 divs_name=div_name.find_all('a', {"class":"product-card"}) # type: ignore
                 for div in divs_name:
@@ -25,6 +31,7 @@ def main() -> List[Dict[str, str]] :
                     description = ''
                     link = div['href']
                     list_div.append({'title':title, 'description': description, "link": link, "slug": filename})
+                
                 file.close()
         except:
             print("Failed during : " + filename)
