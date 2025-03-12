ca32bc159d24132bc54522866a0c5d03e1692a30
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-03-12 19:26:51 +0000

    nightspire parser

---
+++ ransomlook/parsers/nightspire.py
@@ -0,0 +1,25 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        if filename.startswith(__name__.split('.')[-1]+'-'):
+            html_doc='source/'+filename
+            file=open(html_doc,'r')
+            soup=BeautifulSoup(file,'html.parser')
+            divs_name=soup.find_all('div', {"class": "company-item"})
+            for div in divs_name:
+                title = div.find('div',{"class": "name"}).text.strip()
+                description = ""
+                try:
+                    link = div.find('button')['onclick']
+                    list_div.append({'title' : title, 'description': description, 'link': link, 'slug': filename})
+
+                except:
+                    list_div.append({'title' : title, 'description': description})
+            file.close()
+    print(list_div)
+    return list_div
