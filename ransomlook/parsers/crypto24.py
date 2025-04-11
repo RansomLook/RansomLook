d01c3b8412b4df54a744f6d4ea3970f4a1beb894
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-04-11 13:33:47 +0000

    Fixing crypto24

--- ransomlook/parsers/crypto24.py
+++ ransomlook/parsers/crypto24.py
@@ -1,6 +1,7 @@
 import os
 from bs4 import BeautifulSoup
 from typing import Dict, List
+import json
 
 def main() -> List[Dict[str, str]] :
     list_div=[]
@@ -11,14 +12,23 @@ def main() -> List[Dict[str, str]] :
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
                 soup=BeautifulSoup(file,'html.parser')
-                body=soup.find('tbody')
-                divs_name=body.find_all('tr')
-                print(divs_name)
-                for div in divs_name:
-                    tds = div.find_all('td')
-                    title = tds[1].text.strip()
-                    description =  ''
-                    list_div.append({'title':title, 'description': description})
+                if '-data' in filename:
+                    jsonpart= soup.pre.contents # type: ignore
+                    print(jsonpart)
+                    data = json.loads(jsonpart[0]) # type: ignore
+                    print(data)
+                    for entry in data:
+                        title = entry['company']
+                        description = entry['comment']
+                        list_div.append({'title':title, 'description': description})
+                else :
+                    body=soup.find('tbody')
+                    divs_name=body.find_all('tr') # type: ignore
+                    for div in divs_name:
+                        tds = div.find_all('td')
+                        title = tds[1].text.strip()
+                        description =  ''
+                        list_div.append({'title':title, 'description': description})
                 file.close()
         except:
             print("Failed during : " + filename)
