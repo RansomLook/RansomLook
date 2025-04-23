1d830e10738a9e6d99423bf2d6aab520bf5db1b9
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-04-23 12:07:26 +0000

    Silent parser

---
+++ ransomlook/parsers/silent.py
@@ -0,0 +1,24 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "_companieInfoCard_48fxr_1"})
+                for div in divs_name:
+                    title = div.find('strong').text.strip()
+                    description = ""
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
