2060a647741267e537d4518fb2dc426c635e62d2
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-05-13 13:48:20 +0000

    Adding worldleaks

---
+++ ransomlook/parsers/worldleaks.py
@@ -0,0 +1,33 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        if filename.startswith(__name__.split('.')[-1]+'-'):
+            html_doc='source/'+filename
+            file=open(html_doc,'r')
+            soup=BeautifulSoup(file,'html.parser')
+            divs_name=soup.find_all('div', {"class": "wrapper ng-star-inserted"})
+            for div in divs_name:
+              try:  
+                title = div.find('div', {"class": "title"}).text.strip()
+                description = ''
+                link = div.a['href']
+                list_div.append({'title': title, 'description': description, 'link': link, 'slug': filename})
+              except:
+                pass
+            divs_name=soup.find_all('div', {"class": "wrapper ng-star-inserted selected"})
+            for div in divs_name:
+              try:  
+                title = div.find('div', {"class": "title"}).text.strip()
+                description = ''
+                link = div.a['href']
+                list_div.append({'title': title, 'description': description, 'link': link, 'slug': filename})
+              except:
+                pass
+            file.close()
+    print(list_div)
+    return list_div
