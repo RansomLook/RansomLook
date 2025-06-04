8195500cb72a91b4869cc72ccf394a4bd1cbfa15
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-06-04 09:51:04 +0000

    Global parser

---
+++ ransomlook/parsers/global.py
@@ -0,0 +1,24 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"style":"margin-top: 0px; font-family: var(--font-sans);"})
+                for div in divs_name:
+                    title = div.find('h2').text.strip()
+                    description = div.p.text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
