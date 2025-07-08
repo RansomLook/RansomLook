eef1a13c9601d0859d749bf080f27e0ff62df7fa
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-07-08 05:46:14 +0000

    Kawa parser

---
+++ ransomlook/parsers/kawa.py
@@ -0,0 +1,30 @@
+import os
+import re
+from typing import Dict, List
+import json
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                js_content = file.read()
+                if 'leaks-data' in filename:
+                    match = re.search(r"let\s+leaks\s*=\s*(\[[\s\S]*?\])\s*,", js_content)
+                    if match:
+                        leaks_json = match.group(1)
+                        leaks_json_clean = re.sub(r'(\{|,)\s*(\w+)\s*:', r'\1"\2":', leaks_json)
+                        data = json.loads(leaks_json_clean)
+                        for entry in data:
+                            title = entry['title'].strip()
+                            description = entry['description'].strip()
+                            list_div.append({'title':title, 'description': description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+
+    return list_div
