e7172875e3e50740a516af5f37f0589fa5c23d61
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-07-18 16:02:12 +0000

    Blackbyte Crux parser

---
+++ ransomlook/parsers/blackbyte-crux.py
@@ -0,0 +1,24 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('table', {"class": "table table-bordered table-content"})
+                for div in divs_name:
+                    title = div.find('h1').text.strip()
+                    description = div.find('p').text.strip()
+                    list_div.append({'title':title, 'description': description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
