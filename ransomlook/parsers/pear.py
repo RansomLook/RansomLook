6860669c78f40a2ac94cb4e682941b2dbcd7c4a3
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-08-06 07:01:20 +0000

    Pear parser

---
+++ ransomlook/parsers/pear.py
@@ -0,0 +1,22 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        if filename.startswith(__name__.split('.')[-1]+'-'):
+            html_doc='source/'+filename
+            file=open(html_doc,'r')
+            soup=BeautifulSoup(file,'html.parser')
+            
+            divs_name=soup.find_all('details')
+            for div in divs_name:
+                title = div.find('u').text.strip()
+                description = div.find('p').text.strip()
+                link =  div.find('a', {"class":"es-button"})['href']
+                list_div.append({'title': title, 'description': description, "link": link, "slug": filename})
+            file.close()
+    print(list_div)
+    return list_div
