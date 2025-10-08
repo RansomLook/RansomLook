23472e691385ac6117e7f7657ffdd964ef0883e8
Fafner [_KeyZee_] <fkz@p4nd4.land>
2025-10-08 07:30:48 +0000

    Fixing Chaos

--- ransomlook/parsers/chaos.py
+++ ransomlook/parsers/chaos.py
@@ -1,4 +1,5 @@
 import os
+import json
 from bs4 import BeautifulSoup
 from typing import Dict, List
 
@@ -11,12 +12,23 @@ def main() -> List[Dict[str, str]] :
                 html_doc='source/'+filename
                 file=open(html_doc,'r')
                 soup=BeautifulSoup(file,'html.parser')
-                divs_name=soup.find_all('div', {"class": "w-full rounded-lg max-w-md bg-gray-900"})
-                for div in divs_name:
-                    title = div.find('a').text.strip()
-                    description = div.find('div',{"class": "whitespace-pre-line break-words"}).text.strip()
-                    link = div.find('a')['href']
-                    list_div.append({'title':title, 'description': description, 'link': link, 'slug': filename})
+                if 'api' in filename:
+                    jsonpart= soup.pre.contents # type: ignore
+                    data = json.loads(jsonpart[0]) # type: ignore
+                    for entry in data['items']:
+                      if 'hash' in entry:
+                        title = entry['title'].strip()
+                        description = entry['text'].strip()
+                        link = '/'+entry['hash']+'/'+entry['link']
+                        list_div.append({'title':title, 'description': description, 'link': link, 'slug': filename})
+
+                else:
+                    divs_name=soup.find_all('div', {"class": "w-full rounded-lg max-w-md bg-gray-900"})
+                    for div in divs_name:
+                        title = div.find('a').text.strip()
+                        description = div.find('div',{"class": "whitespace-pre-line break-words"}).text.strip()
+                        link = div.find('a')['href']
+                        list_div.append({'title':title, 'description': description, 'link': link, 'slug': filename})
                 file.close()
 
         except:
