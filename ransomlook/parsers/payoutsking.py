d3867f9c19b03221370cd9dbdb5817fc268b517b
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2025-12-16 05:02:15 +0100

    Fixing payoutsking

--- ransomlook/parsers/payoutsking.py
+++ ransomlook/parsers/payoutsking.py
@@ -12,16 +12,22 @@ def main() -> List[Dict[str, str]] :
                 file=open(html_doc,'r')
                 soup=BeautifulSoup(file,'html.parser')
                 tbody=soup.find('tbody', {"id": "table"})
-                trs = tbody.find_all('tr') # type: ignore
-                for tr in trs:
-                    try:
-                        tds = tr.find_all('td')
-                        title = tds[1].text.strip()
-                        description = tds[2].text.strip()
-                        link=tds[1].a['hx-post']
-                        list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
-                    except:
-                        pass
+                if tbody:
+                    trs = tbody.find_all('tr') # type: ignore
+                    for tr in trs:
+                        try:
+                            tds = tr.find_all('td')
+                            title = tds[1].text.strip()
+                            description = tds[2].text.strip()
+                            link=tds[1].a['hx-post']
+                            list_div.append({"title" : title, "description" : description, "link": link, "slug": filename})
+                        except:
+                            pass
+                companies = soup.find_all('company-chevron')
+                print(companies)
+                for company in companies:
+                    title = company.text.strip()
+                    list_div.append({"title" : title, "description" : ""})
                 file.close()
 
         except:
