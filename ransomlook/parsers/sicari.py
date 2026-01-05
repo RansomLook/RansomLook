e6ccf48046e5c3647463abc650ea1f015bdd1c72
Fafner [_KeyZee_] <fkz@p4nd4.land>
2026-01-05 14:36:43 +0000

    Sicari parser

---
+++ ransomlook/parsers/sicari.py
@@ -0,0 +1,27 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "blog-post"})
+                for div in divs_name:
+                    title = div.find('h2').text.strip()
+                    descriptions = div.find_all('p')
+                    description=''
+                    for p in range(0,2):
+                        description +=descriptions[p].text.strip()
+                    list_div.append({"title" : title, "description" : description})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
