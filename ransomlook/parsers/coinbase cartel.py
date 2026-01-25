80a7fa10f424c2e4469163bfa7cb054515bd5445
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2026-01-25 17:50:06 +0100

    Add files via upload

--- ransomlook/parsers/coinbase cartel.py
+++ ransomlook/parsers/coinbase cartel.py
@@ -17,6 +17,12 @@ def main() -> List[Dict[str, str]] :
                     description = div.find('div', {"class": "card-meta"}).text.strip()
                     link = div.find('a', {"class": "view-detail"})['href']
                     list_div.append({"title" : title, "description" : description, "link" : link, "slug": filename})
+                divs_name=soup.find_all('article', {"class": "company-row"})
+                for div in divs_name:
+                    title = div.find('h3').text.strip()
+                    description = ""
+                    link = div.find('a', {"class": "btn btn-primary"})['href']
+                    list_div.append({"title" : title, "description" : description, "link" : link, "slug": filename})
                 file.close()
         except:
             print("Failed during : " + filename)
