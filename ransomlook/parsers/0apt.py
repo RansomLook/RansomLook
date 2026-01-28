e78f7c86da73e9bec1c10576c56921df164fba67
Fafner [_KeyZee_] <fkz@p4nd4.land>
2026-01-28 08:04:33 +0000

    0apt parser

---
+++ ransomlook/parsers/0apt.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith(__name__.split('.')[-1]+'-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                divs_name=soup.find_all('div', {"class": "card active"})
+                for div in divs_name:
+                    title = div.find('h3').text.strip()
+                    list_div.append({"title" : title, "description" : ""})
+                file.close()
+        except:
+            print("Failed during : " + filename)
+            pass
+    print(list_div)
+    return list_div
