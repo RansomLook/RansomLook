6a4d3d2109075b8993dc59162909c74d5302cdea
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2026-02-24 13:29:21 +0100

    Fix atomsilo parser

--- ransomlook/parsers/atomsilo.py
+++ ransomlook/parsers/atomsilo.py
@@ -10,14 +10,24 @@ def main() -> List[Dict[str, str]] :
             html_doc='source/'+filename
             file=open(html_doc,'r')
             soup=BeautifulSoup(file,'html.parser')
+            ### old template
             divs_name=soup.find_all('h4', {"class": "post-announce-name"})
 
             for div in divs_name:
                 for item in div.contents[1]:
                     list_div.append(item.text.strip())
+            ###
+
+            divs_name=soup.find_all('div', {"class": "leak-card"})
+
+            for div in divs_name:
+                title =  div.find('h3').text.strip()
+                descriptions =div.find_all('li')
+                description = " ".join(li.get_text(strip=True) for li in descriptions)
+                link = div.find('a')["href"]
+                list_div.append({"title": title, "description": description.strip(), "link": link, "slug": filename})
 
             file.close()
 
-    list_div = list(dict.fromkeys(list_div))
     print(list_div)
     return list_div
