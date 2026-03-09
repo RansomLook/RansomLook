eb22829c8fdd655a76fa6102bf7f14ef3c324cc1
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2026-03-09 16:19:23 +0100

    Ailock parser
    Implement main function to parse HTML files and extract news card titles.

---
+++ ransomlook/parsers/ailock.py
@@ -0,0 +1,20 @@
+import os
+from bs4 import BeautifulSoup
+from typing import Dict, List
+
+def main() -> List[Dict[str, str]] :
+    list_div=[]
+
+    for filename in os.listdir('source'):
+        if filename.startswith(__name__.split('.')[-1]+'-'):
+            html_doc='source/'+filename
+            file=open(html_doc,'r')
+            soup=BeautifulSoup(file,'html.parser')
+            divs_name=soup.find_all('div', {"class": "news-card"})
+            for div in divs_name:
+                title = div.find('h3').text.strip()
+                description = ''
+                list_div.append({'title' : title, 'description': description})
+            file.close()
+    print(list_div)
+    return list_div
