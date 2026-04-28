c899cd26101bf2aa922b5724b8bc4e434f499177
Fafner [_KeyZee_] <fkz@p4nd4.land>
2026-04-28 13:59:39 +0000

    mnt6 parser

---
+++ ransomlook/parsers/mnt6.py
@@ -0,0 +1,29 @@
+import os
+
+from bs4 import BeautifulSoup
+
+from ransomlook.default.logging import get_logger
+
+logger = get_logger(__name__)
+
+
+def main() -> list[dict[str, str]]:
+    list_div = []
+
+    for filename in os.listdir("source"):
+        try:
+            if filename.startswith(__name__.split(".")[-1] + "-"):
+                html_doc = "source/" + filename
+                file = open(html_doc, encoding="utf-8")
+                soup = BeautifulSoup(file, "html.parser")
+                divs_name = soup.find_all("div", {"class": "card"})
+                for div in divs_name:
+                    title = div.find('h2').text.strip()
+                    description = div.find("p").text.strip()
+                    link = div['onclick'].split("'")[1]
+                    list_div.append({"title": title, "description": description, "link": link, "slug": filename})
+                file.close()
+        except Exception:
+            logger.debug("Failed during : " + filename)
+    logger.debug(list_div)
+    return list_div
