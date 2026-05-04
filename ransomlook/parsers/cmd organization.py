3e5d65bb9b3beaa70d1c141e1f8d84494010b010
Fafner [_KeyZee_] <fkz@p4nd4.land>
2026-05-04 11:00:21 +0000

    Cmd organization parser

---
+++ ransomlook/parsers/cmd organization.py
@@ -0,0 +1,34 @@
+import os
+
+from bs4 import BeautifulSoup
+
+from ransomlook.default.logging import get_logger
+
+logger = get_logger(__name__)
+
+
+def main() -> list[dict[str, str]]:
+    list_div = []
+
+    for filename in os.listdir("source"):
+        try:
+            if filename.startswith(__name__.split(".")[-1] + "-"):
+                html_doc = "source/" + filename
+                file = open(html_doc, encoding="utf-8")
+                soup = BeautifulSoup(file, "html.parser")
+                divs_name = soup.find_all("div", {"class": "auction-card"})
+                for div in divs_name:
+                    title = div.find("h2").text.strip()
+                    description = div.find("div", {"class": "auction-description"}).text.strip()
+                    list_div.append({"title": title, "description": description})
+                divs_name = soup.find_all("div", {"class": "item-card"})
+                for div in divs_name:
+                    title = div.find("h2").text.strip()
+                    description = div.find("div", {"class": "description-before"}).text.strip()
+                    list_div.append({"title": title, "description": description})
+
+                file.close()
+        except Exception:
+            logger.debug("Failed during : " + filename)
+    logger.debug(list_div)
+    return list_div
