92664c052134b2dfdcdd93d489509532826cd076
Fafner [_KeyZee_] <fkz@p4nd4.land>
2026-06-02 20:40:25 +0000

    Balck x parser

---
+++ ransomlook/parsers/black x.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+from bs4 import BeautifulSoup
+
+from ransomlook.default.logging import get_logger
+
+logger = get_logger(__name__)
+
+
+def main() -> list[dict[str, str]]:
+    list_div = []
+
+    for filename in os.listdir("source"):
+        try:
+            if filename.startswith(__name__.split(".")[-1] + "-"):
+                html_doc = "source/" + filename
+                file = open(html_doc, encoding="utf-8")
+                soup = BeautifulSoup(file, "html.parser")
+                try:
+                    jsonpart = soup.pre.contents  # type: ignore
+                    data = json.loads(jsonpart[0])  # type: ignore
+                    for entry in data["targets"]:
+                        title = entry["name"]
+                        description = entry["note"].strip()
+                        list_div.append(
+                            {
+                                "title": title,
+                                "description": description,
+                                "link": "/target/" + entry["_id"],
+                                "slug": filename,
+                            }
+                        )
+                except Exception:
+                    pass
+
+                file.close()
+        except Exception:
+            logger.debug("Failed during : " + filename)
+    logger.debug(list_div)
+    return list_div
