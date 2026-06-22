5b3ae7398f6c9f58a672aa8da78305e310bf1e94
Fafner [_KeyZee_] <fkz@p4nd4.land>
2026-06-22 12:18:48 +0000

    Wallstreet parser

---
+++ ransomlook/parsers/wallstreet.py
@@ -0,0 +1,38 @@
+import json
+import os
+
+from bs4 import BeautifulSoup
+
+from ransomlook.default.logging import get_logger
+
+logger = get_logger(__name__)
+
+
+def main() -> list[dict[str, str]]:
+    list_div = []
+
+    for filename in os.listdir("source"):
+        try:
+            if filename.startswith(__name__.split(".")[-1] + "-"):
+                html_doc = "source/" + filename
+                file = open(html_doc, encoding="utf-8")
+                soup = BeautifulSoup(file, "html.parser")
+                raw = soup.pre.text if soup.pre else soup.get_text()  # type: ignore
+                data = json.loads(raw)
+                for entry in data:
+                    title = entry.get("title", "").replace("\n", "").strip()
+                    description = entry.get("description", "").replace("\n", "").strip()
+                    link = entry.get("confidential", "")
+                    list_div.append(
+                        {
+                            "title": title,
+                            "description": description,
+                            "link": link,
+                            "slug": filename,
+                        }
+                    )
+                file.close()
+        except Exception:
+            logger.debug("Failed during : " + filename)
+    logger.debug(list_div)
+    return list_div
